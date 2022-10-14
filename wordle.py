817f78da3129c3910ed0ece1065b166630ae8d73
PTwu06 <peter22293@gamil.com>
2022-10-14 17:55:58 +0800

    compare user input

--- wordle.py
+++ wordle.py
@@ -1,4 +1,14 @@
 import random
 if __name__ =="__main__":
-    #(1) user input
+    #user input
     user_input = input()
+
+
+    #compare user_input
+    for i in range(len(user_input)):
+        if user_input[i] == answer[i]:
+            print("A")
+        elif user_input[i] in answer:
+            print("B")
+        else:
+            print("X")
