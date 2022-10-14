d3685162f4bb58afac0e1d5df6049ba85fac7102
PTwu06 <peter22293@gamil.com>
2022-10-14 17:54:44 +0800

    user input

--- wordle.py
+++ wordle.py
@@ -2,21 +2,3 @@ import random
 if __name__ =="__main__":
     #(1) user input
     user_input = input()
-
-    #(2) specify an answer
-    f = open("words.txt","r")
-    dictionary = f.read().splitlines()
-    f.close()
-    
-    answer = random.sample(dictionary,1)[0]
-    print(answer)
-    
-    #compare user_input
-    for i in range(len(user_input)):
-        if user_input[i] == answer[i]:
-            print("A")
-        elif user_input[i] in answer:
-            print("B")
-        else:
-            print("X")
-
